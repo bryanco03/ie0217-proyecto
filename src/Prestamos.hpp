d9963eda8878bce99b301af7f3db8ec32fa44320
DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>
2024-02-14 18:33:48 -0600

    Documentación de métodos/atributos de Prestamos.hpp para presentación de avance.
    Signed-off-by: DiegoAlfaroUCR <diego.alfarosegura@ucr.ac.cr>

--- src/Prestamos.hpp
+++ src/Prestamos.hpp
@@ -1,3 +1,10 @@
+/**
+ * \copyright Copyright 2024 Diego Alfaro, Bryan Cortes, Heiner Obando. All right reserved. This project is released under the MIT License
+ * @file Prestamos.hpp
+ * @date 14/02/2024
+ * @author Diego Alfaro, Bryan Cortes, Heiner Obando
+*/
+
 #ifndef PRESTAMOS_HPP
 #define PRESTAMOS_HPP
 
@@ -6,20 +13,42 @@
 #include <cmath>
 #include <fstream>
 
+
 class Prestamos{
     private:
-        double monto, cuotaMensual;
-        float tasaInteres;
-        int duracionMeses;
-        std::string tipo;
-        int cuotasPagadas = 0;
+        double monto;           /**< Monto a pedir del Prestamo. */
+        double cuotaMensual;    /**< Cuota mensual a pagar. */
+        float tasaInteres;      /**< Tasa de interés anual del Prestamo. */
+        int duracionMeses;      /**< Cantidad de meses a pagar de cuotas. */
+        std::string tipo;       /**< Tipo de prestamo: Personal, Prendario o Hipotecario. */
+        int cuotasPagadas = 0;  /**< Cantidad de cuotas ya pagadas del Prestamo. */
 
     public:
-        std::string estado;
-        std::string ID;
+        std::string estado;     /**< Estado del Prestamo: En proceso de pago o Pagado. */
+        std::string ID;         /**< Número ID del prestamo. */
+
+        /**
+         * @brief Constructor que inicializa un prestamo.
+         * 
+         * @param monto Monto a pedir del Prestamo.
+         * @param tasaInteres Tasa de interés anual del Prestamo.
+         * @param duracionMeses Cantidad de meses a pagar de cuotas.
+         * @param tipo Tipo de prestamo: Personal, Prendario o Hipotecario.
+         * @param ID Número ID del prestamo.
+         */
         Prestamos(double monto, float tasaInteres, int duracionMeses,
                   std::string tipo, std::string ID);
+
+        /**
+         * @brief Método para pagar una cuota del prestamo.
+         * 
+         */
         void pagarCuota();
+
+        /**
+         * @brief Método para calcular/retornar información del Prestamo.
+         * 
+         */
         void calcular();
 };
 
